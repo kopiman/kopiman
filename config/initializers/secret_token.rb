77b63f4f0b7764ad50e4cb7c934c10a80b748d4a
koko <exst_jarriesgado@yahoo.com>
2012-05-21 09:34:30 +0800

    first chorva

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kopiman::Application.config.secret_token = '8628beb0c391bfb717d987c0cdffd757f3eafe7a2466bbe3fa379b1dac0070db0a9631bba89d12da3dc30abdc2280bb9f30ec1259d106f6a6b5cfd25e76ba2fe'
